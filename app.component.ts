e1fcf1745a66c32476f1264e98671f4ede9d9884
real-primarchvg <primarchvg@gmail.com>
2017-09-12 13:14:43 +0300

    создан компонент hero-detail

--- app.component.ts
+++ app.component.ts
@@ -1,9 +1,6 @@
 import { Component } from '@angular/core';
 
- export class Hero {
-  id: number;
-  name: string;
- }
+import { Hero } from './hero';
 
  const HEROES: Hero[] = [
   {id: 11, name: 'Mrs. Lol'},
@@ -27,17 +24,10 @@ import { Component } from '@angular/core';
     <li *ngFor="let hero of heroes"
     [class.selected]="hero === seletcedHero"
     (click)="onSelect(hero)">
-     <span class="badge">{{hero.id}}</span> {{hero.name}}
+    <span class="badge">{{hero.id}}</span> {{hero.name}}
     </li>
   </ul>
-  <div *ngIf="selectedHero">
-   <h2>{{selectedHero.name}} details!</h2>
-   <div><label>id: </label>{{selectedHero.id}}</div>
-   <div>
-     <label>name: </label>
-     <input [(ngModel)]="selectedHero.name" placeholder="name"/>
-   </div>
-  </div>
+  <hero-detail [hero]="selectedHero"></hero-detail>
   `,
 
   styles: [`
