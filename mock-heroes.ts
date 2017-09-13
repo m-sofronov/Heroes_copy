a81c81b6067e03f2b4757f2054e216a02ce83eb3
real-primarchvg <primarchvg@gmail.com>
2017-09-13 13:22:28 +0300

    Создал компонент, пригодный для повторного использования

---
+++ mock-heroes.ts
@@ -0,0 +1,14 @@
+ import { Hero } from './hero';
+
+ export const HEROES: Hero[] = [
+  {id: 11, name: 'Mrs. Lol'},
+  {id: 12, name: 'Pacman'},
+  {id: 13, name: 'Narko'},
+  {id: 14, name: 'Bombarco'},
+  {id: 15, name: 'Super'},
+  {id: 16, name: 'Bat'},
+  {id: 17, name: 'Spider'},
+  {id: 18, name: 'Iron'},
+  {id: 19, name: 'Two and half'},
+  {id: 20, name: 'Emperor'},
+ ];
